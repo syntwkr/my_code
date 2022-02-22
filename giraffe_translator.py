ef9edc24257c82d3b029b7b21614cdafabe07375
syntwkr <syntwkr@gmail.com>
2022-02-22 21:27:59 +1100

    Upper

--- giraffe_translator.py
+++ giraffe_translator.py
@@ -4,7 +4,10 @@ def translate(phrase):
 
     for letter in phrase:
         if letter.lower() in "aeiou":
-            translation = translation + "g"
+            if letter.isupper():
+                translation = translation + "G"
+            else:
+                translation = translation + "g"
         else:
             translation = translation + letter
     return translation
